4f68a8d30f06a3e0e7dee7ac1170040fc75d6c52
Charles Batista <charlesbatista@hotmail.com>
2024-03-26 22:20:48 -0300

    Iniciando rotas no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './components/styles/global'
 import { defaultTheme } from './components/styles/themes/default'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="warning" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
